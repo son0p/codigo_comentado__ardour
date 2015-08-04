1aca573b104a776a3fc352048a9ce02d57c3b369
Paul Davis <paul@linuxaudiosystems.com>
2015-08-04 14:30:41 -0400

    small rationalization of naming and code for managing plugin GUI visibility

--- gtk2_ardour/processor_box.cc
+++ gtk2_ardour/processor_box.cc
@@ -2839,7 +2839,7 @@ ProcessorBox::edit_processor (boost::shared_ptr<Processor> processor)
 
 	if (proxy) {
 		proxy->set_custom_ui_mode (true);
-		proxy->toggle ();
+		proxy->show_the_right_window ();
 	}
 }
 
@@ -2857,7 +2857,7 @@ ProcessorBox::generic_edit_processor (boost::shared_ptr<Processor> processor)
 
 	if (proxy) {
 		proxy->set_custom_ui_mode (false);
-		proxy->toggle ();
+		proxy->show_the_right_window ();
 	}
 }
 
@@ -3123,13 +3123,16 @@ ProcessorWindowProxy::get (bool create)
 }
 
 void
-ProcessorWindowProxy::toggle ()
+ProcessorWindowProxy::show_the_right_window ()
 {
 	if (_window && (is_custom != want_custom)) {
 		/* drop existing window - wrong type */
 		drop_window ();
+		get (true);
+		setup ();
+		assert (_window);
+		is_custom = want_custom;
 	}
-	is_custom = want_custom;
 
-	WM::ProxyBase::toggle ();
+	toggle ();
 }
